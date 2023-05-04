25c60f6129ca63102f1fb66d2f5e17a3d5968525
racha ouldbabaali <rachaould@gmail.com>
2023-05-05 00:49:05 +0100

    fix remove task

--- dist/bundle.js
+++ dist/bundle.js
@@ -43,9 +43,12 @@ var Store = /*#__PURE__*/function () {
   }, {
     key: "removeTask",
     value: function removeTask(index) {
+      console.log('index to be removed ', index);
       var tasks = this.getTasks();
-      tasks.splice(index, 1);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+      var filteredTasks = tasks.filter(function (ele) {
+        return ele.index != index;
+      });
+      localStorage.setItem('tasks', JSON.stringify(filteredTasks));
     }
 
     // check if task is already in the list
@@ -676,17 +679,13 @@ var UI = /*#__PURE__*/function () {
     value: function addTaskToList(task) {
       var list = document.querySelector('#tasks');
       var row = document.createElement('li');
-      row.innerHTML = "\n        <input type=\"checkbox\" class=\"checkbox\">\n        <div id=".concat(task.index, " class=\"task-description\">").concat(task.description, "</div>\n        <label class=\"label\"><i class=\"fas fa-check\"></i></label>\n        <a href=\"#\" class ='edit' > <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-square edit\" viewBox=\"0 0 16 16\">\n        <path class='edit' d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>\n        <path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>\n        </svg></a>\n        <a href=\"#\" id =\"delete-btn\" ><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3-fill delete\" viewBox=\"0 0 16 16\">\n        <path class=\"deleteFromPath\" d=\"M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z\"/>\n        </svg></a>\n        ");
+      row.innerHTML = "\n        <input type=\"checkbox\" class=\"checkbox\">\n        <div class=\"task-description\">".concat(task.description, "</div>\n        <label class=\"label\"><i class=\"fas fa-check\"></i></label>\n        <a href=\"#\" class ='edit' > <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-square edit\" viewBox=\"0 0 16 16\">\n        <path class='edit' d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>\n        <path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>\n        </svg></a>\n        <a href=\"#\" id =\"delete-btn\" data-task=").concat(task.index, "><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3-fill delete\" viewBox=\"0 0 16 16\">\n        <path class=\"deleteFromPath\" d=\"M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z\"/>\n        </svg></a>\n        ");
       list.appendChild(row);
     }
   }, {
     key: "deleteTask",
     value: function deleteTask(el) {
-      if (el.classList.contains('delete')) {
-        el.parentElement.parentElement.remove();
-      } else if (el.classList.contains('deleteFromPath')) {
-        el.parentElement.parentElement.parentElement.remove();
-      }
+      el.parentNode.remove();
     }
   }, {
     key: "showAlert",
@@ -747,9 +746,12 @@ document.querySelector('#todo-form').addEventListener('submit', function (e) {
 // Event: Remove a Task
 document.querySelector('#tasks').addEventListener('click', function (e) {
   e.preventDefault();
-  UI.deleteTask(e.target);
+  var parentBtn = e.target.closest('#delete-btn');
+  var taskIndex = parentBtn.dataset.task;
+  UI.deleteTask(parentBtn);
   // Remove task from localStorage
-  _modules_store_js__WEBPACK_IMPORTED_MODULE_2__["default"].removeTask(e.target.parentElement.parentElement.children.id);
+
+  _modules_store_js__WEBPACK_IMPORTED_MODULE_2__["default"].removeTask(taskIndex);
 });
 })();
 
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,36 +1,37 @@
 class Store {
-  static getTasks() {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    return tasks;
-  }
-
-  static addTask(task) {
-    const tasks = this.getTasks();
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-
-  static removeTask(index) {
-    const tasks = this.getTasks();
-    tasks.splice(index, 1);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-
-  // check if task is already in the list
-  static checkTask(description) {
-    const tasks = Store.getTasks();
-    let check = false;
-    tasks.forEach((task) => {
-      if (task.description.toUpperCase() === description.toUpperCase()) {
-        check = true;
+    static getTasks() {
+      let tasks;
+      if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+      } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
       }
-    });
-    return check;
+      return tasks;
+    }
+  
+    static addTask(task) {
+      const tasks = this.getTasks();
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  
+    static removeTask(index) {
+      console.log('index to be removed ',index);
+      const tasks = this.getTasks();
+      const filteredTasks = tasks.filter(ele=> {return ele.index!=index})
+      localStorage.setItem('tasks', JSON.stringify(filteredTasks));
+    }
+  
+    // check if task is already in the list
+    static checkTask(description) {
+      const tasks = Store.getTasks();
+      let check = false;
+      tasks.forEach((task) => {
+        if (task.description.toUpperCase() === description.toUpperCase()) {
+          check = true;
+        }
+      });
+      return check;
+    }
   }
-}
-export default Store;
+  export default Store;
