5c3f317d6171f8756b52d8ac55c02bcf7cbf5ecf
racha ouldbabaali <rachaould@gmail.com>
2023-05-05 11:26:04 +0100

    Add Edit method

--- src/index.js
+++ src/index.js
@@ -13,12 +13,8 @@ class UI {
     const row = document.createElement('li');
     row.innerHTML = `
         <input type="checkbox" class="checkbox">
-        <div class="task-description">${task.description}</div>
+        <input class="task-description" id="${task.index}" value= "${task.description}">  
         <label class="label"><i class="fas fa-check"></i></label>
-        <a href="#" class ='edit' > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square edit" viewBox="0 0 16 16">
-        <path class='edit' d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-        </svg></a>
         <a href="#" id ="delete-btn" data-task=${task.index}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill delete" viewBox="0 0 16 16">
         <path class="deleteFromPath" d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
         </svg></a>
@@ -26,9 +22,8 @@ class UI {
     list.appendChild(row);
   }
 
-  
   static deleteTask(el) {
-    el.parentNode.remove()
+    el.parentNode.remove();
   }
 
   static showAlert(message, className) {
@@ -43,6 +38,12 @@ class UI {
 
   static clearFields() {
     document.querySelector('#todo-input').value = '';
+  }
+
+  static editTask(index) {
+    const task = Store.getTasks(index);
+    const input = document.querySelector('.task-description');
+    task.description = input.value;
   }
 }
 
@@ -88,7 +89,17 @@ document.querySelector('#tasks').addEventListener('click', (e) => {
   const parentBtn = e.target.closest('#delete-btn');
   const taskIndex = parentBtn.dataset.task;
   UI.deleteTask(parentBtn);
-    // Remove task from localStorage
+  // Remove task from localStorage
+  Store.removeTask(taskIndex);
+});
 
-    Store.removeTask(taskIndex);
+// Event: Edit a Task on change
+document.querySelector('#tasks').addEventListener('change', (e) => {
+  e.preventDefault();
+  // get the index of the task
+  const index = e.target.id;
+  UI.editTask(index, e.target.value);
+  UI.showAlert('Task Updated', 'success');
+  // Update task in localStorage
+  Store.updateTask(index, e.target.value);
 });
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,37 +1,52 @@
 class Store {
-    static getTasks() {
-      let tasks;
-      if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-      } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-      }
-      return tasks;
+  static getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
     }
-  
-    static addTask(task) {
-      const tasks = this.getTasks();
-      tasks.push(task);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-  
-    static removeTask(index) {
-      console.log('index to be removed ',index);
-      const tasks = this.getTasks();
-      const filteredTasks = tasks.filter(ele=> {return ele.index!=index})
-      localStorage.setItem('tasks', JSON.stringify(filteredTasks));
-    }
-  
-    // check if task is already in the list
-    static checkTask(description) {
-      const tasks = Store.getTasks();
-      let check = false;
-      tasks.forEach((task) => {
-        if (task.description.toUpperCase() === description.toUpperCase()) {
-          check = true;
-        }
-      });
-      return check;
-    }
+    return tasks;
   }
-  export default Store;
+
+  static addTask(task) {
+    const tasks = this.getTasks();
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    tasks.forEach((task) => {
+      task.index = tasks.indexOf(task) + 1;
+    });
+  }
+
+  static removeTask(index) {
+    const tasks = this.getTasks();
+    const filteredTasks = tasks.filter((ele) => ele.index != index);
+    localStorage.setItem('tasks', JSON.stringify(filteredTasks));
+  }
+
+  // check if task is already in the list
+  static checkTask(description) {
+    const tasks = Store.getTasks();
+    let check = false;
+    tasks.forEach((task) => {
+      if (task.description.toUpperCase() === description.toUpperCase()) {
+        check = true;
+      }
+    });
+    return check;
+  }
+
+  // update task with the new description
+  static updateTask(index, description) {
+    const tasks = Store.getTasks();
+    tasks.forEach((task) => {
+      if (task.index == index) {
+        task.description = description;
+      }
+
+      task.index = tasks.indexOf(task) + 1;
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
+export default Store;
